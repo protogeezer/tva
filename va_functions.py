eab39ce3b5ab2397796e289a22f7a6188e77a28e
lizs <elizabeth.santorella@gmail.com>
2015-10-31 19:17:00 -0400

    Sped up basic_va_alg

--- va_functions.py
+++ va_functions.py
@@ -14,15 +14,15 @@ def estimate_var_epsilon(data):
     return var_epsilon_hat
     
 def get_average(df, var_name, group_name):
-    return df.groupby(group_name)[var_name].apply(lambda group: group.mean() + 0 * group)
+    grouped = df.groupby(group_name)[var_name]
+    return grouped.apply(lambda group: group.mean() + 0 * group)
     
 # Demeaning with one or two fixed effects 
 def demean(df, var_name, first_group, second_group = None):
     if second_group is None:
-        return df[var_name] - get_average(df, var_name, first_group)
-    else:
-        return df[var_name] - get_average(df, var_name, first_group) \
-               - get_average(df, var_name, second_group) + np.mean(df[var_name])
+        return df[var_name] - get_average(df, var_name, first_group)  
+    return df[var_name] - get_average(df, var_name, first_group) \
+           - get_average(df, var_name, second_group) + np.mean(df[var_name])
 
 
 # Calculates beta using y_name = x_names * beta + group_name (dummy) + dummy_control_name
@@ -30,27 +30,27 @@ def demean(df, var_name, first_group, second_group = None):
 def residualize(df, y_name, x_names, first_group, second_group = None):
     if len(x_names) == 0 and second_group is None:
         return df[y_name], None
-    else:
-        if len(x_names) == 0:
-           return demean(df, y_name, second_group) + np.mean(df[y_name]), None
-        else:      
-            # Drop missing values to calculate beta
-            df_no_null = df[[y_name, first_group] + x_names].dropna() if second_group is None \
-                         else df[[y_name, first_group, second_group] + x_names].dropna()
 
-            Y = np.array(demean(df_no_null, y_name, first_group, second_group))
-            X = np.transpose([demean(df_no_null, x, first_group, second_group)
-                          for x in x_names])
-            try:
-                beta = linalg.lstsq(X, Y)[0]
-            except ValueError:
-                raise Exception('Your covariates may be too large relative to the length of your data. This may happen after dropping many null values.')
-            
-            if second_group is None:
-                return df[y_name] - np.dot(df[x_names], beta), beta 
-            else:
-                x_demeaned = np.transpose([demean(df, x, second_group) + np.mean(df[x]) for x in x_names])
-                return demean(df, y_name, second_group) + np.mean(df[y_name]) - np.dot(x_demeaned, beta), beta
+    if len(x_names) == 0:
+        return demean(df, y_name, second_group) + np.mean(df[y_name]), None
+     
+    # Drop missing values to calculate beta
+    df_no_null = df[[y_name, first_group] + x_names].dropna() if second_group is None \
+                 else df[[y_name, first_group, second_group] + x_names].dropna()
+
+    Y = np.array(demean(df_no_null, y_name, first_group, second_group))
+    X = np.transpose([demean(df_no_null, x, first_group, second_group)
+                  for x in x_names])
+    try:
+        beta = linalg.lstsq(X, Y)[0]
+    except ValueError:
+        raise Exception('Your covariates may be too large relative to the length of your data. This may happen after dropping many null values.')
+    
+    if second_group is None:
+        return df[y_name] - np.dot(df[x_names], beta), beta 
+    else:
+        x_demeaned = np.transpose([demean(df, x, second_group) + np.mean(df[x]) for x in x_names])
+        return demean(df, y_name, second_group) + np.mean(df[y_name]) - np.dot(x_demeaned, beta), beta
 
 
 def get_teacher_class_map(data, teachers):
