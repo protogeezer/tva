8bc90d175d2c836c2882c51608d9024ff77e3eae
lizs <elizabeth.santorella@gmail.com>
2015-12-04 17:08:33 -0500

    Sped up by using Numpy until the last line.

--- simulate_baseline.py
+++ simulate_baseline.py
@@ -1,48 +1,38 @@
-from math import sqrt
 import numpy as np
-import random
 import pandas as pd
-import multiprocessing
 
-num_teachers = 10000
-beta = [2, 3]
-sd_epsilon = sqrt(.2455)
-# approximate mean classes taught; zero is not allowed
-parameters = {'sd mu':sqrt(.0135), 'sd theta':sqrt(.0295), 'mean class size':20, 'mean classes taught':3}
+#parameters = {'sd mu':.0135**.5, 'sd theta':.0295**.5, 'mean class size':20, 'mean classes taught':3, 'num teachers': 1000, 'sd epsilon':.2455**.5, 'beta':[2, 3]}
 
-def generate_one_teacher_data(input_tuple):   
-    teacher_id, params = input_tuple   
-    teacher_df = []
-    mu = np.random.normal(0, params['sd mu'])
-    for class_id in range(max(1, np.random.poisson(params['mean classes taught']))):
-        theta = np.random.normal(0, params['sd theta'])
-        class_size = round(np.random.poisson(params['mean class size']))        
-        class_df = pd.DataFrame()  
-           
-        class_df['student id'] = range(class_size)
-        class_df['class id'] = class_id
-        class_df['score'] = theta
+def simulate(params):
+    num_teachers = params['num teachers']
+    beta = params['beta']
+    sd_epsilon, sd_mu, sd_theta = params['sd epsilon'], params['sd mu'], params['sd theta']
+    mean_class_size, mean_classes_taught = params['mean class size'], params['mean classes taught']
     
-        teacher_df.append(class_df)
+    def simulate_one_teacher(teacher_id):
+        classes = range(max(1, np.random.poisson(mean_classes_taught)))
+        sizes = np.random.poisson(mean_class_size, len(classes))
         
-    teacher_df = pd.concat(teacher_df)
-    teacher_df['teacher'] = teacher_id
-    teacher_df['true va'] = mu
-    teacher_df['score'] = teacher_df['score'] + mu
+        class_id_vector = np.hstack((np.tile(class_id, size) 
+                                    for class_id, size in zip(classes, sizes)))
+        # teacher, class id, student id, true va, score, x1, x2
+        # 0     , 1         , 2         , 3     , 4     , 5, 6
+        data = np.empty((len(class_id_vector), 7))
+        data[:, 0] = teacher_id
+        data[:, 1] = class_id_vector
+        data[:, 2] = np.hstack((range(s) for s in sizes))
+        mu = np.random.normal(0, sd_mu)
+        data[:, 3] = mu
+        theta_vector = np.hstack((np.tile(np.random.normal(0, sd_theta), s)
+                                  for s in  sizes))
+        data[:, 4] = mu + theta_vector
         
-    return teacher_df
-    
-start = time.time()
-num_cores = multiprocessing.cpu_count()
-pool = multiprocessing.Pool(num_cores)
-dfs = pool.map(generate_one_teacher_data, [(i, parameters) for i in range(num_teachers)])
-pool.close()
-pool.join()
-
-dfs = pd.concat(dfs)
-dfs['x1'] = [np.random.normal() for i in range(len(dfs))]
-dfs['x2'] = [np.random.normal() for i in range(len(dfs))]
-dfs['score'] = dfs['score'] + np.dot(dfs[['x1', 'x2']].as_matrix(), beta) + np.random.normal(0, sd_epsilon, len(dfs))
-
-dfs.to_csv('baseline_simulated_data.csv')
-print(str(time.time() - start))
+        return data
+        
+    data = np.vstack((simulate_one_teacher(i) for i in range(num_teachers)))
+    data[:, 5] = np.random.normal(0, 1, len(data))
+    data[:, 6] = data[:, 3] + np.random.normal(0, 1, len(data))
+    data[:, 4] = data[:, 4] + np.random.normal(0, sd_epsilon, len(data)) \
+                 + np.dot(data[:, 5:7], beta)
+        
+    return pd.DataFrame(data, columns=('teacher', 'class id', 'student id', 'true va', 'score', 'x1', 'x2'))
