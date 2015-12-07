Uses simulations to test whether parameter estimates are right on average, and whether standard errors are correct. Right now, the parameter estimates check out but I haven't run enough simulations to be sure about the standard errors.

evaluate.py
@@ -1,65 +1,71 @@
+import sys
 import pandas as pd
-import basic_va_alg
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
 import numpy as np
-from save_figure import save
-import va_functions
+from basic_va_alg import calculate_va
+from multiprocessing import cpu_count, Pool
+from simulate_baseline import simulate
+import matplotlib.pyplot as plt
+from va_functions import binscatter
 
-# Basic algorithm: Effect is constant within teacher
-directory = '~/Documents/tva/algorithm/'
-filename = 'baseline_simulated_data'
-output_file = open('output_baseline', 'w')
+def compute_estimates_once(i):
+    print(i)
+    params = {'sd mu':.0135**.5, 'sd theta':.0295**.5, 'mean class size':20, 'mean classes taught':3, 'num teachers': 1000, 'sd epsilon':.2455**.5, 'beta':[2, 3]}
+    data = simulate(params)
+    data.loc[:, 'year'] = data['class id']
+    return calculate_va(data, ['x1', 'x2'], False, categorical_controls='year', moments_only=True)
 
-data = pd.read_csv(directory+filename+'.csv', sep=',', nrows = 500000)
-output_file.write('Number of observations: ' + str(len(data.index)))
+n_iters = 4
 
-data.loc[:, 'year'] = data['class id']
+num_cores = min(cpu_count(), n_iters)
+pool = Pool(num_cores)
+results = pool.map(compute_estimates_once, range(n_iters))
+pool.close()
+pool.join()
 
-# Run the algorithm
-[data, var_mu_hat, var_theta_hat, var_epsilon_hat] \
-    = basic_va_alg.calculate_va(data, ['x1', 'x2'], True, parallel=True)
+results = np.array(results)
 
-#data.to_csv('tva_basic_alg_'+filename)
+np.save('parameter_estimates/baseline_'+sys.argv[1]+'.npy', results)
 
-## Summary stats that help check if it is working
-#output_file.write('\nVariance of epsilon, true: ' + str(.2455))
-#output_file.write('\nVariance of epsilon, computed:'+str(var_epsilon_hat))
+############
 
-#output_file.write('\n\nVariance of value-added, true: ' + str(.0135))
-#output_file.write('\nVariance of value-added, computed:'+str(var_mu_hat))
+#results = np.load('parameter_estimates/baseline_0.npy')
 
-#output_file.write('\n\nVariance of theta, true: ' + str(.0295))
-#output_file.write('\nVariance of theta, computed: '+str(var_theta_hat))
+def bins(y, bin_size):
+    return np.arange(min(y), max(y)+bin_size, bin_size)
 
-#output_file.write('\n\nempirical mean of value-added '+str(np.ma.average(data['va'].values)))
-#output_file.write('\nempirical variance of value-added '+str(np.var(data['va'].values)))
 
-## Make plots to show that va is correct on average
-#print('about to binscatter')
-#va_bins, predictions = va_functions.binscatter(data['va'], data['true va'], 20)
-#plt.plot(va_bins, predictions)
+def parameter_hist(y, param_name, true_value):
+    bin_size = .001
+    fig, ax = plt.subplots(1)
+    ax.hist(y, bins=bins(y, bin_size))
+    ax.set_title('Estimates of '+param_name)
+    
+    ax.axvline(true_value, color='k', linewidth='3') # true value
+    ax.axvline(np.mean(y), color='r', linewidth='1') # mean
+    # 95% CI of mean
+    se1 = (np.var(y)/len(y))**(.5)
+    ax.axvline(np.mean(y) - 1.96*se1, color='r', linewidth='1')
+    ax.axvline(np.mean(y) + 1.96*se1, color='r', linewidth='1')
+    print('red lines:')
+    print(np.mean(y))
+    print(np.mean(y) - 1.96*se1)
+    print(np.mean(y) + 1.96*se1)
+    # 5th perentile
+    ax.axvline(sorted(y)[int(.05*len(y))], color='g', linewidth='1')
+    # 95th perentile
+    ax.axvline(sorted(y)[int(.95*len(y))], color='g', linewidth='1')
+    print('green lines:')
+    print(sorted(y)[int(.05*len(y))])
+    print(sorted(y)[int(.95*len(y))])
+        
+    fig.savefig('parameter_estimates/'+param_name+'_sim_estimates_ac')
 
-#plt.plot(np.arange(-.3, .3, .1), np.arange(-.3, .3, .1))
+params = {'sd mu':.0135**.5, 'sd theta':.0295**.5, 'mean class size':20, 'mean classes taught':3, 'num teachers': 1000, 'sd epsilon':.2455**.5, 'beta':[2, 3]}
 
-#plt.show()
+parameter_hist(results[:, 0], 'variance of mu', .0135)
+parameter_hist(results[:, 1], 'variance of theta', .0295)
+parameter_hist(results[:, 2], 'variance of epsilon', .2455)
 
-## Test confidence interval
-#data.loc[:, 'outside ci'] = [true_va > va + 1.96 * se or true_va < va - 1.96 * se 
-#                                       for true_va, va, se in zip(data['true va'].values, \
-#                                           data['va'].values, np.sqrt(data['variance'].values))]
-
-### Regressions
-#result = sm.OLS(data['mean score'], data['va'], hasconst = True, missing = 'drop')
-#result = result.fit()
-#output_file.write('\n'+str(result.summary()))   
-
-## Binned scatter plot
-#class_level_data['missing'] = pd.isnull(class_level_data['va']+class_level_data['va other'])
-#data_nomissing = class_level_data[~class_level_data['missing']]
-#plt.hist2d(data_nomissing['va'].as_matrix(), data_nomissing['va other'].as_matrix(), bins=100)
-#save('figures/2d_scatter_tva_basic_'+ filename)
-
-#bins, y = binscatter(data_nomissing['va'].as_matrix(), data_nomissing['va other'].as_matrix(), 100)
-#plt.plot(bins, y, 'o')
-#save('figures/1d_scatter_basic_'+filename)
+# Test average squared error, normalized by standard deviation
+error = (results[:, 0] - .0135)**2 / results[:, 3]**2
+print(np.mean(error))