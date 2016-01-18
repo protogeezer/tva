e73e5b8af84befa5ffc315b1ab6941d33625b5bb
lizs <elizabeth.santorella@gmail.com>
2016-01-17 21:40:36 -0500

    Minor changes.

--- evaluate.py
+++ evaluate.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import basic_va_alg
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
-import numpy as np
-from save_figure import save
-import va_functions
 
 # Basic algorithm: Effect is constant within teacher
-directory = '~/Documents/tva/algorithm/'
+directory = '~/Dropbox/lizs_backup/Documents/tva/algorithm/'
 filename = 'baseline_simulated_data'
 output_file = open('output_baseline', 'w')
+data = pd.read_csv(directory+filename+'.csv', sep=',')
+print('number of observations:')
+print(len(data))
+#output_file.write('Number of observations: ' + str(len(data.index)))
 
-data = pd.read_csv(directory+filename+'.csv', sep=',', nrows = 500000)
-output_file.write('Number of observations: ' + str(len(data.index)))
-
-data.loc[:, 'year'] = data['class id']
+# discretize x1  
+data.loc[:, 'x1'] = [int(x > 0) for x in data['x1']] + data['true va']
 
 # Run the algorithm
-[data, var_mu_hat, var_theta_hat, var_epsilon_hat] \
-    = basic_va_alg.calculate_va(data, ['x1', 'x2'], True, parallel=True)
+data, var_mu_hat, var_theta_hat, var_epsilon_hat, se, n \
+    = basic_va_alg.calculate_va(data, ['x2'], False, column_names={'year':'class id'}, categorical_controls=['x1'])
 
-#data.to_csv('tva_basic_alg_'+filename)
+data.to_csv('tva_basic_alg_'+filename)
 
-## Summary stats that help check if it is working
-#output_file.write('\nVariance of epsilon, true: ' + str(.2455))
-#output_file.write('\nVariance of epsilon, computed:'+str(var_epsilon_hat))
+# Summary stats that help check if it is working
+output_file.write('\nVariance of epsilon, true: ' + str(.2455))
+output_file.write('\nVariance of epsilon, computed:'+str(var_epsilon_hat))
 
-#output_file.write('\n\nVariance of value-added, true: ' + str(.0135))
-#output_file.write('\nVariance of value-added, computed:'+str(var_mu_hat))
+output_file.write('\n\nVariance of value-added, true: ' + str(.0135))
+output_file.write('\nVariance of value-added, computed:'+str(var_mu_hat))
 
-#output_file.write('\n\nVariance of theta, true: ' + str(.0295))
-#output_file.write('\nVariance of theta, computed: '+str(var_theta_hat))
+output_file.write('\n\nSe of variance of value-added: ' + str(se))
+
+output_file.write('\n\nVariance of theta, true: ' + str(.0295))
+output_file.write('\nVariance of theta, computed: '+str(var_theta_hat))
 
 #output_file.write('\n\nempirical mean of value-added '+str(np.ma.average(data['va'].values)))
 #output_file.write('\nempirical variance of value-added '+str(np.var(data['va'].values)))
+output_file.close()
 
 ## Make plots to show that va is correct on average
 #print('about to binscatter')
