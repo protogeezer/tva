270f07dfc2e2bac4fe8b01f56eff54d2e499e10d
lizs <elizabeth.santorella@gmail.com>
2016-01-17 21:20:53 -0500

    Now returns bootstrapped confidence interval for var_mu_hat instead of SE.

--- basic_va_alg.py
+++ basic_va_alg.py
@@ -1,6 +1,5 @@
 from va_functions import *
 import pandas as pd
-import copy
 import warnings
 import random
 
@@ -12,13 +11,15 @@ def estimate_mu_variance(data):
             return [score_1 * score_2, 1]
         except ValueError:
             return [0, 0]
+            
+    def weighted_mean(arr):
+        return np.sum(arr[:, 0]) / np.sum(arr[:, 1])
         
     mu_estimates = np.array(list(data.groupby('teacher')['mean score'].apply(f).values))
-    n = np.sum(mu_estimates[:, 1])
-    mu_hat = np.sum(mu_estimates[:, 0]) / n
-    se = (np.sum(mu_estimates[:, 0]**2) - mu_hat**2) / (n-1)
-    return mu_hat, se
-
+    mu_hat = weighted_mean(mu_estimates)
+    
+    bootstrap_samples = [weighted_mean(get_bootstrap_sample(mu_estimates)) for i in range(1000)]
+    return mu_hat, [np.percentile(bootstrap_samples, 2.5), np.percentile(bootstrap_samples, 97.5)]
 
 def get_each_va(df, var_theta_hat, var_epsilon_hat, var_mu_hat, jackknife):
     def f(data):
@@ -40,14 +41,13 @@ def get_each_va(df, var_theta_hat, var_epsilon_hat, var_mu_hat, jackknife):
 # moments contains 'var epsilon', 'var mu', 'cov mu', 'var theta', 'cov theta'
 # Column names can specify 'class id', 'student id', and 'teacher'
 # class_level_vars can contain any variables are constant at the class level and will stay in the final data set
-@profile
 def calculate_va(data, covariates, jackknife, residual=None, moments=None, column_names=None, class_level_vars=['teacher', 'class id'], categorical_controls = [], moments_only = False):
     ## First, a bunch of data processing
     if moments is None:
         moments = {}
 
     # Fix column names
-    if column_names is not None:
+    if column_names is not None:              
         data.rename(columns=column_names, inplace=True)
 
     # If a residual was not included, residualize scores
@@ -86,10 +86,11 @@ def calculate_va(data, covariates, jackknife, residual=None, moments=None, colum
     if 'var mu' in moments:
         var_mu_hat = moments['var mu']
     else:
-        var_mu_hat, var_mu_hat_se = estimate_mu_variance(class_df)
+        var_mu_hat, var_mu_hat_ci = estimate_mu_variance(class_df)
     if var_mu_hat <= 0:
         warnings.warn('Var mu hat is negative. Measured to be ' + str(var_mu_hat))
         var_mu_hat = 0
+        
 
     # Estimate variance of class-level shocks
     var_theta_hat = moments.get('var theta', ssr - var_mu_hat - var_epsilon_hat)
@@ -98,10 +99,10 @@ def calculate_va(data, covariates, jackknife, residual=None, moments=None, colum
         var_theta_hat = 0
 
     if moments_only:
-        return var_mu_hat, var_theta_hat, var_epsilon_hat
+        return var_mu_hat, var_theta_hat, var_epsilon_hat, var_mu_hat_ci
         
     results = get_each_va(class_df, var_theta_hat, var_epsilon_hat, var_mu_hat, jackknife)
     if column_names is not None:
         results.rename(columns={column_names[key]:key for key in column_names}, inplace=True)
-                   
-    return results, var_mu_hat, var_theta_hat, var_epsilon_hat, var_mu_hat_se, 0
+    
+    return results, var_mu_hat, var_theta_hat, var_epsilon_hat, var_mu_hat_ci
