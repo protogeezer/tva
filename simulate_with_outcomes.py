6b948c9200b4372e03a9f50460ef7c26519f3a01
lizs <elizabeth.santorella@gmail.com>
2016-05-23 16:56:54 -0400

    Creates simulated data for testing VA algorithm or doing permutation tests.
    Preserved from initial data:
    -- Which state bureaucrat a bureaucrat is in.
    -- How long each bureaucrat has been in her current district.
    -- Whether the bureaucrat is present in this year.
    
    Not preserved:
    -- *Which* district within the state the bureaucrat is in.

--- simulate_with_outcomes.py
+++ simulate_with_outcomes.py
@@ -2,73 +2,59 @@ from va_functions import remove_duplicates
 import numpy as np
 import pandas as pd
 from random import shuffle
+from copy import copy
 
 """
-TODO: Would be better to create balanced panel outside this function. Would be even better to have a check_balanced function that only creates a balanced panel if it is missing.
+Creates simulated data for testing VA algorithm or doing permutation tests.
+
+Preserved from initial data:
+-- Which state bureaucrat a bureaucrat is in.
+-- How long each bureaucrat has been in her current district.
 """
 
-# df should have columns person and district
-# nan = not in sample, 1 = switching to new district, 0 = continuing
+# TODO: Deal with consecutiveness. A posting is all years a bureaucrat works in a certian district.
+
+# binary: was the bureaucrat serving in the same district last year?
 def create_switch_status(df):
-    districts = df[district].values
+    districts = df['clean district name'].values
     
-    df['switch status'] = [current if type(current) == float 
-                                   and np.isnan(current) 
-                                  else int(current != last)
-                           for current, last
+    df['switch status'] = [int(current != last) for current, last
                    in zip(districts, np.concatenate(([0], districts[:-1])))]
     return df    
 
 # df holds data for one state in one year
 def shuffle_bureaucrats(df):
     switch_indices = pd.Series(df['switch status'] == 1)
-    #print(switch_indices)
-    switch_people = df.loc[switch_indices, person].values
+    switch_people = df.loc[switch_indices, 'person'].values
     shuffle(switch_people)
-    #print(switch_people)
-    df.loc[switch_indices, person] = switch_people
+    df.loc[switch_indices, 'person'] = switch_people
     return df
+    
+def find_swappers(df):
+    switch_indices = pd.Series(df['switch status'] == 1)
+    switch_people = df.loc[switch_indices, 'person'].values
+    new_switch_people = copy(switch_people)
+    shuffle(new_switch_people)
+    return dict(zip(switch_people, new_switch_people))
 
-def simulate(df, seed_increment, outcomes, district='district',
-             person='person', time='year', state='state'):
+def simulate(df, seed_increment, outcomes):
+             
     np.random.seed(seed_increment)
+    df = df.groupby('person').apply(create_switch_status)
+    times = sorted(set(df['year'].values))
     
-    district = 'clean district name'
-    person = 'person'
-    time = 'year'
-    state = 'state'
-    df = pd.read_csv('/home/lizs/Documents/ias/data/indicus_cleaned.csv')
-    df.drop('Unnamed: 0', axis=1, inplace=True)
-    df[person] = [int(x) for x in df[person]]
-    
-    # Create a balanced panel by first making complete panel, then merging
-    """
-    times = sorted(set(df[time].values))
-    T = len(times)
-    collectors = remove_duplicates(df[person].values)
-    N = len(collectors)
-    
-    balanced_panel = pd.DataFrame(np.array([np.tile(collectors, T)
-                                          , np.array(times).repeat(N)]).T
-                                , columns=[person, time])
-    df = df.merge(balanced_panel, how = 'right')
-    """
-    """
-     Give each collector a "status" variable tells whether they switch,
-     enter, or exit
-     e.g. [NA, A, A, B, B, C, NA]
-      ->  [NA, 'enter', 'continue', 'switch', 'continue', 'switch', 'exit']
-    """
-    df = df.groupby(person).apply(create_switch_status)
-    
-    """
-    Also track a state variable for each bureaucrat
-
-    Randomly order bureacurats so that "status" and "state" stay the same,
-    but district *within* a state is random.
-    """
-    print(df[df[district] == 'Adilabad'])
-    df = df.groupby((state, time)).apply(shuffle_bureaucrats)
-    print(df[df[district] == 'Adilabad'])
-    
+    # TODO: Speed up by doing swaps within a state, then looking for unmatched
+    # people elsewhere
+    for t in times[1:]:
+        # find out who needs to swap with who
+        # first column of swaps is original, second is replacement
+        swaps = df[df['year'] == t].groupby('state').apply(find_swappers).values
+        swaps = {k:v for d in swaps for k, v in d.items()}
+        
+        # Replace bureaucrats in all future times
+        indices = [s >= t and p in swaps for s, p in 
+                   df[['year', 'person']].values]
+        df.loc[indices, 'person'] = [swaps[p] 
+                                   for p in df.loc[indices, 'person'].values]
+        
     return df
