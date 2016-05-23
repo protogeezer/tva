ec2e7eeb60e8ebe5cefb66cf01d5fe093e95c512
lizs <elizabeth.santorella@gmail.com>
2016-05-23 15:34:01 -0400

    Now deals with zero variances. Stopped inadvertently changing types.

--- simulate_with_assignments.py
+++ simulate_with_assignments.py
@@ -1,5 +1,4 @@
 import sys
-sys.path = sys.path + ['/home/lizs/Documents/tva/algorithm/']
 from va_functions import remove_duplicates
 import numpy as np
 import pandas as pd
@@ -9,6 +8,9 @@ import pandas as pd
 # such that if identifiers[x] = identifiers[y],
 # effects[x] = effects[y]
 def fill_effects(identifiers, st_dev):
+    if st_dev == 0:
+        return np.zeros(len(identifiers))
+
     no_dup_ids = remove_duplicates(identifiers)
     id_effect_dict = dict(zip(no_dup_ids
                         , np.random.normal(0, st_dev, len(no_dup_ids))))
@@ -23,15 +25,21 @@ def simulate(params, assignments, seed_increment):
     
     var_epsilon = 1 - var_theta - var_mu
     std_epsilon = var_epsilon**.5
+    month_type = type(assignments['month_id'].values[0])
     
-    assignments['delta'] = fill_effects(assignments['distcode'].values, var_delta**.5)
-    assignments['mu']    = fill_effects(assignments['person'].values, var_mu**.5)
-    postings = list(zip(assignments['distcode'].values, assignments['person'].values))
+    
+    assignments['delta'] = fill_effects(assignments['distcode'].values
+                                      , var_delta**.5)
+    
+    assignments['mu'] = fill_effects(assignments['person'].values, var_mu**.5)
+    postings = list(zip(assignments['distcode'].values
+                       , assignments['person'].values))
     assignments['theta'] = fill_effects(postings, var_theta**.5)
     
     ## Create panel in which districts are always there
     # And use it to create serial correlation
     districts = remove_duplicates(assignments['distcode'].values)
+
     times = remove_duplicates(assignments['month_id'].values)
     T = len(times)
     D = len(districts)
@@ -43,18 +51,16 @@ def simulate(params, assignments, seed_increment):
     all_errors[0, :] = current_error
     
     for t in range(1, T):
-        current_error = rho*current_error + np.random.normal(0, var_epsilon**.5, D)
+        current_error = rho * current_error + np.random.normal(0, var_epsilon**.5, D)
         all_errors[t, :] = current_error
-        
-        
-    balanced_panel = pd.DataFrame(np.array([
-                                             np.tile(districts, T)
-                                           , np.array(times).repeat(D)
-                                           , all_errors.flatten()
-                                          ]).T
-                        , columns=['distcode', 'month_id', 'error'])
-        
+
+
+    balanced_panel = pd.DataFrame({'distcode': np.tile(districts, T),
+                                   'month_id': np.array(times).repeat(D),
+                                   'error': all_errors.flatten()})
 
     assignments = pd.merge(assignments, balanced_panel, how='left')
-    assignments['outcome'] = assignments['mu'] + assignments['delta'] + assignments['theta'] + assignments['error']
+
+    assignments['outcome'] = assignments['mu'] + assignments['delta'] \
+                         + assignments['theta'] + assignments['error']
     return assignments
