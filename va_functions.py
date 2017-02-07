f754d3e3e90b4b7943b8d464fa657da32e9c512b
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-02-07 16:32:23 -0500

    Removed unused functions

--- va_functions.py
+++ va_functions.py
@@ -138,44 +138,6 @@ def estimate_var_epsilon(data):
     assert var_epsilon_hat > 0
     return var_epsilon_hat
     
-# Demeaning with one fixed effect
-def fe_demean(df, variables, group):
-    f = lambda df: df - np.mean(df.values, axis = 0)
-        
-    return df.groupby(group)[variables].apply(f)
-
-# Calculates beta using y_name = x_names * beta + group_name (dummy) + dummy_control_name
-# Returns               y_name - x_names * beta - dummy_control_name
-def residualize(df, y_name, x_names, first_group, second_groups = None):
-    y = df[y_name].values
-    
-    if len(x_names) == 0 and second_groups is None: # don't do anything
-        return y - np.mean(y), [np.mean(y)]
-    else:
-        x = df[x_names].values
-        x_demeaned = fe_demean(df, x_names, first_group)
-        
-        if second_groups is not None: # If extra FE, create dummies
-            dummy_df = pd.get_dummies(df, columns=second_groups)
-            dummy_cols = []
-            for col in second_groups:
-                dummy_cols += [elt for elt in dummy_df.columns if col in elt]
-                
-            dummies = dummy_df[dummy_cols].values
-                                
-            if len(x_names) > 0:
-                x = np.hstack((x, dummies))
-                x_demeaned = np.hstack((x_demeaned, dummies))
-            else:
-                x = dummies
-                x_demeaned = dummies
-
-        beta = linalg.lstsq(x_demeaned, y)[0]
-        print('Beta from original ', beta[:10])
-        resid = y - np.dot(x, beta)
-        print('Resid from original ', resid[:10])
-        return resid - np.mean(resid), beta
-                
 
 # Mean 0, variance 1
 def normalize(vector):
@@ -197,14 +159,6 @@ def remove_duplicates(seq):
     return result
 
 
-def drop_one_class_teachers(class_df):
-    grouped = class_df.groupby('teacher')['class id']
-    df = pd.DataFrame(grouped.apply(lambda x: len(x) > 1).reset_index())
-    df.columns = ['teacher', 'keep']
-    class_df = pd.merge(df, class_df)
-    return class_df[class_df['keep']].drop('keep', axis=1)
-
-
 def binscatter(x, y, nbins):
     assert len(x) == len(y)
     # sort according to x
@@ -220,13 +174,13 @@ def binscatter(x, y, nbins):
     y_95 = np.zeros(nbins)
     
     for i in range(0, nbins):
-        start = len(x) * i / nbins
-        end = len(x) * (i+1) / nbins
-        bins[i] = np.mean(x[int(start):int(end)])
-        y_means[i] = np.mean(y[int(start):int(end)])
-        y_medians[i] = np.median(y[int(start):int(end)])
-        y_5[i] = np.percentile(y[int(start):int(end)], 5)
-        y_95[i] = np.percentile(y[int(start):int(end)], 95)
+        start = int(len(x) * i / nbins)
+        end = int(len(x) * (i+1) / nbins)
+        bins[i] = np.mean(x[start:end])
+        y_means[i] = np.mean(y[start:end])
+        y_medians[i] = np.median(y[start:end])
+        y_5[i] = np.percentile(y[start:end], 5)
+        y_95[i] = np.percentile(y[start:end], 95)
 
     return bins, y_means, y_medians, y_5, y_95
 
@@ -272,8 +226,3 @@ def get_va(df, var_theta_hat, var_epsilon_hat, var_mu_hat, jackknife):
         return_val = [[num_sum / denominator, 1 / denominator]]
 
     return pd.DataFrame(data=np.array(return_val))
-
-
-def get_bootstrap_sample(myList):
-    indices = np.random.choice(range(len(myList)), len(myList))
-    return myList[indices]
