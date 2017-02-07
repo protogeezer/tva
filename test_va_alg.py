7334cad5431262b61ff789e10ea50d0ae71a9983
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-02-07 16:31:52 -0500

    Now tests three different estimators

--- test_va_alg.py
+++ test_va_alg.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from basic_va_alg_2 import calculate_va
-from basic_va_alg import calculate_va as calculate_va_orig
+from basic_va_alg import calculate_va
 
 
 sigma_mu_squared = 3
@@ -16,22 +15,12 @@ data['outcome'] = -7 + data['fiscal year'] * 3 + person_effects[person]
 data['constant'] = 1
 
 # Try each of three different estimators
-print('Original')
-est_orig = calculate_va_orig(data.copy(), ['fiscal year', 'constant'], False, 
-        column_names={'class id': 'clean district name', 'outcome':'score', 
-            'fiscal year':'fiscal year', 'constant':'constant', 'person':'teacher'}, moments_only=True,
-        categorical_controls=['class id'])
-print('\n\n\n KS')
 est_ks = calculate_va(data.copy(), 'outcome', 'person', ['fiscal year'],
                       ['person', 'clean district name'], categorical_controls=['clean district name'],
                       method='ks', add_constant=False)
-print('\n\n\n CFR')
 est_cfr = calculate_va(data.copy(), 'outcome', 'person', ['fiscal year'],
                       ['person', 'clean district name'], categorical_controls=['clean district name'],
                       method='cfr', add_constant=False)
-print(est_orig)
-print(est_ks)
-print(est_cfr)
-#est_fk = calculate_va(data.copy(), 'outcome', 'person', ['fiscal year'],
-#                      ['person', 'clean district name'], categorical_controls=['clean district name'],
-#                      method='fk')
+est_fk = calculate_va(data.copy(), 'outcome', 'person', ['fiscal year'],
+                      ['person', 'clean district name'], categorical_controls=['clean district name'],
+                      method='fk')
