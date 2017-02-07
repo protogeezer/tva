e6804507ddeb40ea1a6dcabc6c55ec3dc256b839
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-02-07 16:31:33 -0500

    Added categorical data; recoded some variables as int

--- simulate_baseline.py
+++ simulate_baseline.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-def simulate(params):
-    np.random.seed()
+def simulate(params, seed):
+    np.random.seed(seed)
     num_teachers = params['num teachers']
     beta = params['beta']
     sd_epsilon, sd_mu, sd_theta = params['sd epsilon'], params['sd mu'], params['sd theta']
@@ -17,7 +17,7 @@ def simulate(params):
                                     for class_id, size in zip(classes, sizes)))
         # teacher, class id, student id, true va, score, x1, x2
         # 0     , 1         , 2         , 3     , 4     , 5, 6
-        data = np.empty((len(class_id_vector), 7))
+        data = np.empty((len(class_id_vector), 8))
         data[:, 0] = teacher_id
         data[:, 1] = class_id_vector
         data[:, 2] = np.hstack((range(s) for s in sizes))+100*class_id_vector+10000*teacher_id
@@ -32,10 +32,20 @@ def simulate(params):
     data = np.vstack((simulate_one_teacher(i) for i in range(num_teachers)))
     data[:, 5] = np.random.normal(0, 1, len(data))
     data[:, 6] = data[:, 3] + np.random.normal(0, 1, len(data))
-    data[:, 4] = data[:, 4] + np.random.normal(0, sd_epsilon, len(data)) \
-                 + np.dot(data[:, 5:7], beta)
-        
-    return pd.DataFrame(data, columns=('teacher', 'class id', 'student id', 'true va', 'score', 'x1', 'x2'))
+    # Create categorical data
+    categorical = np.random.choice(range(5), len(data))
+    data[:, 7] = categorical
+    fixed_effects = np.random.normal(0, 1, 5)
+    data[:, 4] += np.random.normal(0, sd_epsilon, len(data)) \
+                 + np.dot(data[:, 5:7], beta)\
+                 + fixed_effects[categorical]
+    data = pd.DataFrame(data, columns=('teacher', 'class id', 'student id', 'true va', 'score', 'x1', 'x2', 'c1'))
+    for col in ['student id', 'teacher', 'class id', 'c1']:
+        data[col] = data[col].astype(int)
+    return data
+
     
 if __name__=="__main__":
-    simulate({'num teachers':1000, 'beta':[2, 3], 'sd mu':.024**.5, 'sd theta':.178**.5, 'sd epsilon':(1-.024-.178)**.5, 'mean class size':20, 'mean classes taught':3}).to_csv('baseline_simulated_data_1.csv')
+    simulate({'num teachers':1000, 'beta':[2, 3], 'sd mu':.024**.5, 'sd theta':.178**.5, 
+              'sd epsilon':(1-.024-.178)**.5, 'mean class size':20, 'mean classes taught':3},
+              2).to_csv('monte_carlo_data.csv', index=False)
