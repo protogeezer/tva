31f58fcf6efc73a2b4ba505a3716d32247279b3b
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-05-26 14:27:24 -0400

    LL for one teacher works now

--- mcmc_likelihood_check.py
+++ mcmc_likelihood_check.py
@@ -5,21 +5,23 @@ import sys
 sys.path += [hdfe_dir]
 from hdfe import Groupby
 from scipy.stats import norm
+from scipy.stats import multivariate_normal as mvn
 
 sigma_theta = 2
 n_classes = 2
-n_students = 11
+n_students = 110
 np.random.seed(52617)
 theta = np.random.normal(0, sigma_theta, n_classes)
-class_ids = np.random.choice(range(2), 5)
+class_ids = np.random.choice(range(2), n_students)
 mu = 1
 sigma_epsilon = 3
 y = np.random.normal(mu, sigma_epsilon, n_students) + theta[class_ids]
 
 # Hyperparamters to try
-mu = [3, 5]
-sigma_epsilon = [1, 4]
-sigma_theta = [1, 2]
+mu = [3, -3]
+sigma_epsilon = [7, 4]
+sigma_theta = [1, 3]
+sigma_mu = [3, 5]
 
 
 # f and g are equivalent
@@ -58,78 +60,45 @@ def get_ll_one_class(y,  mu, sigma_epsilon, sigma_theta):
          + norm.logpdf(np.sqrt(np.sum(y_tilde**2)), 0, sigma_epsilon)
 
     sigma_epsilon_over_n = sigma_epsilon / np.sqrt(len(y))
-    theta_samples = np.random.normal(0, sigma_theta, 10**6)
+    theta_samples = np.random.normal(0, sigma_theta, 10**4)
     return ll + np.log(np.mean(norm.pdf(y_bar - mu - theta_samples, 0, sigma_epsilon_over_n)))
 
 
-def get_ll_one(y, class_ids, mu, sigma_epsilon, sigma_theta):
+def get_ll(y, class_ids, mu, sigma_epsilon, sigma_theta):
     ll = 0
     for i in set(class_ids):
         idx = class_ids == i
         ll += get_ll_one_class(y[idx], mu, sigma_epsilon, sigma_theta)
     return ll
 
+def get_ll_teacher(y, class_ids, sigma_mu, sigma_epsilon, sigma_theta):
+    mu_samples = np.random.normal(0, sigma_mu, 10**4)
+    return np.log(np.mean([np.exp(get_ll(y, class_ids, mu, sigma_epsilon, sigma_theta))
+                          for mu in mu_samples]))
 
-def get_ll_two(y, class_ids, mu, sigma_epsilon, sigma_theta):
-    y_bar = np.mean(y)
-    y_tilde = y - y_bar
-    sqrt_ssr = np.sqrt(np.sum(y_tilde**2))
+def get_ll_two(y, class_ids, sigma_mu, sigma_epsilon, sigma_theta):
+    grouped = Groupby(class_ids)
+    y_bar = grouped.apply(np.mean, y, broadcast=False)
+    y_tilde = y - y_bar[class_ids]
+    n_students = grouped.apply(len, y, broadcast=False)
+    h = 1 / (sigma_epsilon**2 / n_students + sigma_theta**2)
+    y_bar_bar = y_bar.dot(h) / np.sum(h)
+    y_bar_tilde = y_bar - y_bar_bar
 
-    ll = (2 - len(y)) * np.log(sigma_epsilon) + norm.logpdf(sqrt_ssr, 0, sigma_epsilon)
-    h = np.sqrt(sigma_epsilon**2 / len(y) + sigma_theta**2)
-    return ll + norm.logpdf(y_bar, mu, h) 
+
+    ll = (1 + grouped.n_keys - len(y)) * np.log(sigma_epsilon)
+    ll += norm.logpdf(np.sqrt(np.sum(y_tilde**2)), 0, sigma_epsilon)
+    ll += .5 * np.sum(np.log(h)) - .5 * np.log(np.sum(h)) - .5 * h.dot(y_bar_tilde**2)
+    ll += norm.logpdf(y_bar_bar, 0, np.sqrt(sigma_mu**2 + 1 / np.sum(h)))
+
+    return ll
 
 start = time.clock()
-ll_one = [get_ll(y, mu[i], sigma_epsilon[i], sigma_theta[i]) for i in [0, 1]]
+ll_one = [get_ll_teacher(y, class_ids, sigma_mu[i], sigma_epsilon[i], sigma_theta[i]) for i in [0, 1]]
 print(time.clock() - start)
 start = time.clock()
-ll_two = [get_ll_two(y, mu[i], sigma_epsilon[i], sigma_theta[i]) for i in [0, 1]]
+ll_two = [get_ll_two(y, class_ids, sigma_mu[i], sigma_epsilon[i], sigma_theta[i]) for i in [0, 1]]
 print(time.clock() - start)
 
 print(ll_one[0] - ll_two[0] - (ll_one[1] - ll_two[1]))
-
-
-# Model: y_i ~ N(mu, sigma_epsilon); set sigma_epsilon = 1
-model_one = """
-data {
-    int<lower=1> n_students; // number of students
-    real y[n_students];     // outcomes
-    // int<lower=1> n_classes;
-    // int<lower=1, upper=n_classes> class_id[n_students];
-}
-parameters {
-    real mu;
-    real<lower=0> sigma_epsilon;
-    // real theta[n_classes];
-    real theta;
-}
-
-model {
-    theta ~ normal(0, 1);
-    y ~ normal(mu + theta, sigma_epsilon);
-    // for (i in 1:n_students)
-    //     y[i] ~ normal(mu + theta[class_id[i]], sigma_epsilon);
-}
-"""
-
-model_one_alt = """
-data {
-    int<lower=0> n_students; // number of students
-    real ssr;
-    real y_bar;
-}
-parameters {
-    real mu;
-    real<lower=0> sigma_epsilon;
-}
-transformed parameters {
-    real<lower=0> sqrt_h;
-    sqrt_h = sqrt(1 + square(sigma_epsilon) / n_students);
-}
-model {
-    ssr ~ normal(0, sigma_epsilon);
-    y_bar ~ normal(mu, sqrt_h);
-    target += (3 - n_students) * log(sigma_epsilon) + log(1) - log(sqrt_h);
-}
-"""
 
