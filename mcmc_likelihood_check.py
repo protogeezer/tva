7c862bffb47010941e52bec5bc92af3777340300
Elizabeth Santorella <elizabeth.santorella@gmail.com>
2017-05-30 15:03:08 -0400

    Works now

--- mcmc_likelihood_check.py
+++ mcmc_likelihood_check.py
@@ -65,41 +65,54 @@ def get_ll_total(y, teacher_idx, class_idx, sigma_mu, sigma_epsilon,
 
 def get_ll_two_teacher(y, class_ids, sigma_mu, sigma_epsilon, sigma_theta):
     grouped = Groupby(class_ids)
-    y_bar = grouped.apply(np.mean, y, broadcast=False)
-    y_tilde = grouped.apply(lambda x: x - np.mean(x), y)
     n_students = grouped.apply(len, y, broadcast=False)
     h = 1 / (sigma_epsilon**2 / n_students + sigma_theta**2)
+
+    y_bar = grouped.apply(np.mean, y, broadcast=False)
     y_bar_bar = y_bar.dot(h) / np.sum(h)
     y_bar_tilde = y_bar - y_bar_bar
 
-
-    ll = (grouped.n_keys - len(y)) * np.log(sigma_epsilon)
-    ll -= np.sum(y_tilde**2) / (2 * sigma_epsilon**2)
-    ll -= .5 * np.log(sigma_mu**2 + 1 / np.sum(h))
-    ll -= y_bar_bar**2 / (2 * (sigma_mu**2 + 1 / np.sum(h)))
-    ll += .5 * np.sum(np.log(h)) - .5 * np.log(np.sum(h)) - .5 * h.dot(y_bar_tilde**2)
-    return ll
+    return -.5 * h.dot(y_bar_tilde**2)
 
 
 def get_ll_two_total(y, teacher_ids, class_ids, sigma_mu, sigma_epsilon,
                      sigma_theta):
-    ll = 0 
-    for i in set(teacher_ids):
-        idx = teacher_ids == i
-        ll += get_ll_two_teacher(y[idx], class_ids[idx], sigma_mu,
-                                 sigma_epsilon, sigma_theta)
+    # TODO: make sure class_ids are unique within teacher
+    class_grouped = Groupby(class_ids)
+    class_ids_unique = class_grouped.apply(lambda x: len(set(x[:, 0])) == 1, 
+                                           teacher_ids)
+    assert class_ids_unique.all()
+    n_classes = class_grouped.n_keys
+    y_tilde = class_grouped.apply(lambda x: x - np.mean(x), y)
+    n_students = class_grouped.apply(len, y, broadcast=False)
+    h = 1 / (sigma_epsilon**2 / n_students + sigma_theta**2)
+
+    teacher_grouped = Groupby(teacher_ids[class_grouped.first_occurrences])
+    h_sum_long = teacher_grouped.apply(np.sum, h)
+    h_sum = h_sum_long[teacher_grouped.first_occurrences]
+
+    y_bar = class_grouped.apply(np.mean, y, broadcast=False)
+    y_bar_bar = teacher_grouped.apply(np.sum, y_bar * h) / h_sum_long
+    y_bar_tilde = y_bar - y_bar_bar[:,0]
+
+    ll = (n_classes - len(y)) * np.log(sigma_epsilon)\
+         - np.sum(y_tilde**2) / (2 * sigma_epsilon**2)\
+         - .5 * np.sum(np.log(sigma_mu**2 + 1 / h_sum))\
+         + .5 * np.sum(np.log(h)) - .5 * np.sum(np.log(h_sum))
+    ll -= .5 * np.sum(y_bar_bar[teacher_grouped.first_occurrences]**2 / (sigma_mu**2 + 1 / h_sum))
+    ll -= .5 * h.dot(y_bar_tilde**2)
     return ll
 
+start = time.clock()
+ll_two = [get_ll_two_total(y, teacher_ids,  class_ids, sigma_mu[i], 
+                           sigma_epsilon[i], sigma_theta[i]) 
+          for i in [0, 1]]
+print(time.clock() - start)
 
 # start = time.clock()
 # ll_one = [get_ll_total(y, teacher_ids, class_ids, sigma_mu[i], sigma_epsilon[i], 
 #                        sigma_theta[i]) 
 #          for i in [0, 1]]
-# print(time.clock() - start)
-# start = time.clock()
-ll_two = [get_ll_two_total(y, teacher_ids,  class_ids, sigma_mu[i], 
-                           sigma_epsilon[i], sigma_theta[i]) 
-          for i in [0, 1]]
 # print(time.clock() - start)
 
 print(ll_one[0] - ll_two[0] - (ll_one[1] - ll_two[1]))
